65e6996402dc2592a196fe336116feff440071c0
Francico Chacon <pacochacon94@gmail.com>
2022-04-28 21:20:08 -0500

    Simplify point.py

--- point.py
+++ point.py
@@ -9,14 +9,22 @@ class Point:
         return self.x, self.y
 
     def distance(self, p):
-        if isinstance(p, Point):
-            x1 = p.x
-            y1 = p.y
-            x2 = self.x
-            y2 = self.y
-            return sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
-        else:
-            print("Point is required")
+        if not isinstance(p, Point): return "Point is required"
+        return sqrt(((p.x - self.x) ** 2) + ((p.y - self.y) ** 2))
 
     def angle(self):
         return atan(self.y / self.x)
+
+
+
+p0 = Point(1.0, 2.0)
+p1 = Point(2.0, 3.0)
+
+print("P0 distance to P1:", p0.distance(p1))
+print("P1 distance to P0:", p1.distance(p0))
+print("P1 wrong distance to 1:", p1.distance(1))
+print("P1 wrong distance to non-point value:", p1.distance("Value"))
+print("P0 angle:", p0.angle())
+print("P1 angle:", p1.angle())
+print("P0 point:", p0.getPoint())
+print("P1 point:", p1.getPoint())
