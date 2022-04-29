947286626e58d53b4e04e6850e8f5a4a62409c3c
Francico Chacon <pacochacon94@gmail.com>
2022-04-28 21:06:17 -0500

    Add class exercise to evaluate some functions
    Evaluate sin(2x) where x = 1.3
    Evaluate exp(1 - 2x) where x = -0.45

---
+++ math_test.py
@@ -0,0 +1,10 @@
+from math import sin, exp
+
+def sin2x(value):
+    return sin(value * 2)
+
+def exp1minus2byx(value):
+    return exp(1 - 2 * value)
+
+print(sin2x(1.3))
+print(exp1minus2byx(-0.45))
