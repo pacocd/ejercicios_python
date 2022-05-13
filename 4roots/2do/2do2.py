01f73bfbaa4456a7992b8c9dc2a80a304a2b7644
Francisco Chacon <pacochacon94@gmail.com>
2022-05-12 19:50:11 -0500

    Add 2do2 from 4th session

--- 4roots/2do/2do2.py
+++ 4roots/2do/2do2.py
@@ -14,3 +14,18 @@
 # it will produce 100 points
 # [1.0, 1.1, ... , 9.9, 10]
 #
+
+def customRange(initialValue, finalValue, increment):
+    counter = initialValue
+    values = []
+    while counter <= finalValue:
+        values.append(counter)
+        counter += increment
+
+    return values
+
+
+print(customRange(1, 3, 0.1))
+print(customRange(5, 7, 0.2))
+print(customRange(5, 7, 0.3))
+print(customRange(1, 4, 0.7))
