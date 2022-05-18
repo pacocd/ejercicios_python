dd29da0c62d69dbbc381396e51a19d25f96f9fb8
Francisco Chacon <pacochacon94@gmail.com>
2022-05-17 21:09:29 -0500

    Move exercises from folder 4 to 3

--- 3interpolation/2do/2do.py
+++ 3interpolation/2do/2do.py
@@ -1,3 +1,5 @@
+import functools
+
 coordinate1 = (-4, -2)
 coordinate2 = (1, 5)
 
@@ -8,3 +10,36 @@ def fn(y, c0, c1):
     return a * b + c0[0]
 
 print(fn(3.7, coordinate1, coordinate2))
+
+
+homework = [(-2, 4), (-1, -2), (3, 5), (4.3, 11)]
+
+def interpolatingPolynomial(x, coordinates):
+    mixedCoords = mixCoords(coordinates)
+    func = lambda stored, iteration: stored + operation(x, mixedCoords[iteration])
+    return functools.reduce(func, range(4), 0)
+
+def mixCoords(coordinates):
+    c0, c1, c2, c3 = coordinates
+    return [
+            coordinates,
+            [c1, c0, c2, c3],
+            [c2, c0, c1, c3],
+            [c3, c0, c1, c2]
+        ]
+
+def operation(x, coordinates):
+    c0 = coordinates[0]
+    return divisionOperation(x, coordinates) * c0[1]
+
+def divisionOperation(x, coordinates):
+    x0, x1, x2, x3 = [coordinate[0] for coordinate in coordinates]
+    dividend = (x - x1) * (x - x2) * (x - x3)
+    divider = (x0 - x1) * (x0 - x2) * (x0 - x3)
+    return dividend / divider
+
+# Ejercicio de Tarea
+
+print("Ejercicio de tarea")
+print(homework, "x =", 7.7)
+print(interpolatingPolynomial(7.7, homework))
