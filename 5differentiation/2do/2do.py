508724efbee43559aa4fdc6b91441fd5947ea9f8
Francisco Chacon <pacochacon94@gmail.com>
2022-05-23 23:32:10 -0500

    Add folder 5

--- 5differentiation/2do/2do.py
+++ 5differentiation/2do/2do.py
@@ -2,3 +2,11 @@
 # refer to PPT file
 # for exercise
 #
+from scipy.misc import derivative
+from math import sin
+
+def f(x):
+    return (sin(2 * x) ** 3) / (x ** 4 + 1)
+
+
+print(derivative(f, 2.45))
